b5d41a3f95ea63901074956015a460877afd8f00
ansh1601 <goyalansh.ag@gmail.com>
2022-01-12 20:12:05 +0530

    Initial commit

---
+++ src/main/java/PubSubToBigQuery.java
@@ -0,0 +1,46 @@
+import com.google.api.services.bigquery.model.TableRow;
+import org.apache.beam.runners.dataflow.DataflowRunner;
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+
+public class PubSubToBigQuery {
+
+
+    public static void main(String[] args){
+        DataflowPipelineOptions dataflowPipelineOptions = PipelineOptionsFactory.as(DataflowPipelineOptions.class);
+        //dataflowPipelineOptions.setJobName("");
+        dataflowPipelineOptions.setProject("nttdata-c4e-bde");
+        dataflowPipelineOptions.setRegion("europe-west4");
+        dataflowPipelineOptions.setGcpTempLocation("gs://c4e-uc1-dataflow-temp-18/temp ");
+        dataflowPipelineOptions.setRunner(DataflowRunner.class);
+
+        Pipeline pipeline= Pipeline.create(dataflowPipelineOptions);
+        PCollection<String> pubsubmessage = pipeline.apply(PubsubIO.readStrings().fromTopic(""));
+        PCollection<TableRow> bqrow=pubsubmessage.apply(ParDo.of(new ConvertorStringBq()));
+
+        bqrow.apply(BigQueryIO.writeTableRows().to("nttdata-c4e-bde:uc1_18.account")
+                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER)
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
+
+
+        pipeline.run();
+    }
+    public static class ConvertorStringBq extends DoFn<String,TableRow>{
+        @ProcessElement
+        public void processing(ProcessContext processContext){
+            TableRow tableRow = new TableRow().set("id",processContext.element()).set("name",processContext.element().toString())
+                    .set("surname",processContext.element().toString());
+
+            processContext.output(tableRow);
+
+
+        }
+
+    }
+}
